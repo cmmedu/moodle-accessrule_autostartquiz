81042d5166cafc56de4d51ec4d9d3d87093dc790
Lozamded <dhiguerafernandez@gmail.com>
2026-01-16 13:46:57 -0300

    ADD: Spanish MX comptibility

---
+++ autostart/lang/es _mx/quizaccess_autostart.php
@@ -0,0 +1,13 @@
+<?php
+
+$string['pluginname'] = 'Regla de acceso: inicio automático';
+$string['autostartheader'] = 'Iniciar automáticamente';
+$string['autostartheader_help'] = 'Configurar opciones de inicio automático para el cuestionario.';
+$string['autostartenabled'] = 'Iniciar el cuestionario automáticamente';
+$string['autostartenabled_help'] = 'Si está activado, el cuestionario se abrirá inmediatamente sin la pantalla intermedia.';
+$string['hidequestionsinfotostudents'] = 'Ocultar información de preguntas a los estudiantes';
+$string['hidequestionsinfotostudents_help'] = 'Si está activado, la información de las preguntas se ocultará a los estudiantes usando CSS.';
+$string['autosend'] = 'Enviar automáticamente al terminar intento';
+$string['autosend_help'] = 'Si está activado, cuando el estudiante presione "Terminar intento", el formulario de "Enviar todo y terminar" se enviará automáticamente.';
+$string['disablerightdrawer'] = 'Desactivar cajón derecho';
+$string['disablerightdrawer_help'] = 'Si está activado, el cajón derecho y su botón de activación se ocultarán.';
